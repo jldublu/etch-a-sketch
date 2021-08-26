54e5ec10cab561bed5ce9c157e71f42bd8c67140
Jonathan Walker <jonwalker302@gmail.com>
2021-08-26 16:42:03 -0400

    Add functionality to change grid size on reset

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-window.onload = createGrid;
+window.onload = createGrid(16);
+window.addEventListener('mouseover', changeBackground);
+document.querySelector('.clear-button').onclick = resetGrid;
 
-window.addEventListener('mouseover', function(e) {
+function changeBackground(e) {
   let element = e.target;
   if (element.classList.contains('grid-square')) {
     element.classList.add('black-background');
   }
+}
 
-});
-
-
-/*
-  Creates a 16 x 16 grid
-*/
-function createGrid() {
+function createGrid(sideLength) {
   const gridContainer = document.querySelector('.grid-container');
-  const gridSize = 16 * 16;
+  gridContainer.style.setProperty('--grid-side', sideLength);
+  const gridSize = sideLength * sideLength;
   for (let i = 0; i < gridSize; i++) {
     const gridSquare = document.createElement('div');
     gridSquare.classList.add('grid-square');
     gridContainer.appendChild(gridSquare);
+  }
+}
+
+function removeGrid() {
+  const gridContainer = document.querySelector('.grid-container');
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+}
+
+function resetGrid() {
+  const changedSquares = document.getElementsByClassName('black-background');
+  while (changedSquares.length > 0) {
+    changedSquares.item(0).classList.remove('black-background');
+  }
+
+  const sideLength = Number(prompt('Please enter a new grid size. (min:5 max:100)'));
+
+  if (Number.isFinite(sideLength) && sideLength >= 5 && sideLength <= 100) {
+    removeGrid();
+    createGrid(sideLength);
   }
 }
